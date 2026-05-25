79e58b353e9251d45b6fb6e0972d845f102bd240
codepuncher <lee.hanbury@gmail.com>
2026-05-25 11:05:27 +0100

    refactor: extract kBestiaryMenuName constant

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -8,6 +8,7 @@ namespace
 	constexpr auto kGfxRestoreSavedSettings = "_root.QuestJournalFader.Menu_mc.RestoreSavedSettings";
 	constexpr auto kGfxSwitchPageToFront = "_root.QuestJournalFader.Menu_mc.SwitchPageToFront";
 	constexpr auto kGfxQJOEndPage = "_root.QuestJournalFader.Menu_mc.QuestsFader.Page_mc.QJO_EndPage";
+	constexpr auto kBestiaryMenuName = "BestiaryMenu";
 }
 
 InputHandler* InputHandler::GetSingleton()
@@ -204,7 +205,7 @@ void InputHandler::DispatchLongPress(const ButtonState& state)
 				menuName = RE::MagicMenu::MENU_NAME;
 				break;
 			case LongPressAction::kBestiary:
-				menuName = "BestiaryMenu";
+				menuName = kBestiaryMenuName;
 				break;
 			default:
 				menuName = RE::InventoryMenu::MENU_NAME;
